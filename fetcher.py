c2e9e6d1e5170bb80196481d0be16b876cedb037
Daniel Kim <danielynskim@gmail.com>
2021-05-20 22:53:03 -0400

    Initial commit

---
+++ fetcher.py
@@ -0,0 +1,21 @@
+import cassiopeia
+
+class Fetcher:
+    def __init__(self):
+        pass
+
+    def fetch_games(self):
+        pass
+
+    def calculate_playtime(self):
+        pass
+    
+    def fetch_mmr(self):
+        # fetch and save
+        pass
+
+    def save_mmr(self):
+        pass
+
+    def add_mmr_summ(self):
+        pass
